f7be088cd0b5079997b4558057d1313f8b8077ff
Brandon Carter <bcarteratx@gmail.com>
2020-02-09 10:38:12 -0600

    strict button working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 /*----- constants -----*/
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+const round = document.querySelector('#message');
+const startButton = document.querySelector('#start');
+const strictButton = document.querySelector("#strict");
+//sound files
 
 /*----- app's state (variables) -----*/
-let compOrder;
-let playerOrder = [];
+let compPattern;
+let playerPattern = [];
+let compTurn;
 let totalRounds = 20;
 let strict = false;
 let playerRound;
@@ -10,23 +19,41 @@ let win;
 /*----- cached element references -----*/
 
 /*----- event listeners -----*/
-document.querySelector('.game').addEventListener('click', handleOrder);
-document.getElementById('reset').addEventListener('click', handleReset);
+document.querySelector('.game').addEventListener('click', handlePattern);
+document.getElementById('start').addEventListener('click', startGame);
 document.getElementById('strict').addEventListener('click', handleStrict);
 
 
 /*----- functions -----*/
-function getOrder() {
-    compOrder = Array.from({length: 20}, () => Math.floor(Math.random() * 4) +1);
-    console.log(compOrder);
+function getPattern() {
+    compPattern = Array.from({length: 1}, () => Math.floor(Math.random() * 4) +1);
 }
 
-function handleOrder() {
-    console.log('lights working');
+function startGame() {
+    document.getElementById('message').textContent = "Round 1";
+    //all lights flash to indicate game start
+
+    getPattern();
+    //playPattern() {light up elements by compPatern}
+    
+    //check for 
 }
-function handleReset() {
-    console.log('reset working');
+
+function matchPattern() {
+    if (playerPattern === compPattern) {
+        playerRound += 1;
+    } else {
+        startPattern();
+    }
 }
+
+function handlePattern(evt) {
+    //when color is clicked input respective value into playerorder array.
+    let lightValue = parseInt(evt.target.textContent);
+    playerPattern.push(lightValue);
+    console.log(playerPattern);
+}
+
 function handleStrict() {
-    console.log('strict working');
-}
+    (strictButton.checked == true) ? strict = true : strict = false; 
+  }
