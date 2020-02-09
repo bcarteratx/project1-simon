c632ec307b9b5e29fc76c70bfd93ba1f952b214c
Brandon Carter <bcarteratx@gmail.com>
2020-02-09 12:10:16 -0600

    set up lightsOn lightsOff class replace

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ const red = document.querySelector('.red');
 const blue = document.querySelector('.blue');
 const yellow = document.querySelector('.yellow');
 const green = document.querySelector('.green');
-const round = document.querySelector('#message');
+const message = document.querySelector('#message');
+const round = document.querySelector('#round');
 const startButton = document.querySelector('#start');
 const strictButton = document.querySelector("#strict");
 //sound files
 
 /*----- app's state (variables) -----*/
-let compPattern;
+let compPattern = [];
 let playerPattern = [];
 let compTurn;
+let compFlash;
 let totalRounds = 20;
 let strict = false;
 let playerRound;
 let win;
+let wrongMove;
+let intervalTime;
 /*----- cached element references -----*/
 
 /*----- event listeners -----*/
@@ -25,27 +29,9 @@ document.getElementById('strict').addEventListener('click', handleStrict);
 
 
 /*----- functions -----*/
-function getPattern() {
-    compPattern = Array.from({length: 1}, () => Math.floor(Math.random() * 4) +1);
-}
-
-function startGame() {
-    document.getElementById('message').textContent = "Round 1";
-    //all lights flash to indicate game start
-
-    getPattern();
-    //playPattern() {light up elements by compPatern}
-    
-    //check for 
-}
-
-function matchPattern() {
-    if (playerPattern === compPattern) {
-        playerRound += 1;
-    } else {
-        startPattern();
-    }
-}
+function handleStrict() {
+    (strictButton.checked == true) ? strict = true : strict = false; 
+  }
 
 function handlePattern(evt) {
     //when color is clicked input respective value into playerorder array.
@@ -54,6 +40,51 @@ function handlePattern(evt) {
     console.log(playerPattern);
 }
 
-function handleStrict() {
-    (strictButton.checked == true) ? strict = true : strict = false; 
-  }
+function getPattern() {
+    compPattern = Array.from({length: 1}, () => Math.floor(Math.random() * 4) +1);
+}
+
+function matchPattern() {
+    Math.abs(playerPattern);
+    Math.abs(compPattern);
+    if (playerPattern === compPattern) {
+        playerRound += 1;
+    } else {
+        //startPattern();
+    }
+}
+
+function startGame() {
+    round.textContent = 1;
+    win = false;
+    wrongMove = false;
+    getPattern();
+    compTurn = true;
+    //all lights flash to indicate game start
+    intervalTime = setInterval(compTurn, 500);
+    lightsOn();
+    lightsOff();
+    //playPattern() {light up elements by compPatern}
+    
+    //check for 
+}
+
+function startRound() {
+
+}
+
+
+
+function lightsOff() {
+    red.classList.replace('on', 'off');
+    blue.classList.replace('on', 'off');
+    yellow.classList.replace('on', 'off');
+    green.classList.replace('on', 'off');
+}
+
+function lightsOn() {
+    red.classList.replace('off', 'on');
+    blue.classList.replace('off', 'on');
+    yellow.classList.replace('off', 'on');
+    green.classList.replace('off', 'on');
+}
